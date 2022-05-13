3ec81434553ada5547d7b6164cb03c8ebb3a6cb6
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-13 19:09:34 -0300

    Adiciona a media aritimetica da soma de todos os valores do array

--- 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-Javascript/Exercicios-mais-complexos/script.js
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-Javascript/Exercicios-mais-complexos/script.js
@@ -3,9 +3,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 console.log(numbers);
 
 let resultado = 0;
+let media = 0;
 
 for(let index=0; index<numbers.length; index+=1){
     resultado += numbers[index];
 }
 
 console.log("A soma de todos os valores do array é:",resultado);
+
+media = resultado / numbers.length;
+
+console.log(media);
