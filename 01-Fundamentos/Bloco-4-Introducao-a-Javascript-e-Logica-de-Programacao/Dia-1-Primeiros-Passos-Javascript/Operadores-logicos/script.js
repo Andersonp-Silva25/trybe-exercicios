8fea6b63dab705482257abea1289cafe69e15942
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-13 11:39:36 -0300

    Adiciona uma variavel para receber o dia da semana

--- 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-Javascript/Operadores-logicos/script.js
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-Javascript/Operadores-logicos/script.js
@@ -1,16 +1,22 @@
-const currentHour = 23;
-let message = "";
+// // Utilizando Operador && (e)
 
-if (currentHour >= 22){
-    message = "Não deveríamos comer nada, é hora de dormir";
-}else if (currentHour >= 18 && currentHour < 22){
-    message = "Rango da noite, vamos jantar :D";
-}else if (currentHour >= 14 && currentHour < 16){
-    message = "Vamos fazer um bolo pro café da tarde?";
-}else if (currentHour >= 11 && currentHour < 14){
-    message = "Hora do almoço!!!";
-}else if (currentHour >= 4 && currentHour < 11){
-    message = "Hmmm, cheiro de café recém passado";
-}
+// const currentHour = 23;
+// let message = "";
 
-console.log(message);
+// if (currentHour >= 22){
+//     message = "Não deveríamos comer nada, é hora de dormir";
+// }else if (currentHour >= 18 && currentHour < 22){
+//     message = "Rango da noite, vamos jantar :D";
+// }else if (currentHour >= 14 && currentHour < 16){
+//     message = "Vamos fazer um bolo pro café da tarde?";
+// }else if (currentHour >= 11 && currentHour < 14){
+//     message = "Hora do almoço!!!";
+// }else if (currentHour >= 4 && currentHour < 11){
+//     message = "Hmmm, cheiro de café recém passado";
+// }
+
+// console.log(message);
+
+// Utilizando Operador || (Ou)
+
+let weekDay = "quarta-feira";
