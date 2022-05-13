98d03195b1897893ff047792275015dd058f50bd
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-13 19:05:13 -0300

    Adiciona a soma para todos os valores do array

--- 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-Javascript/Exercicios-mais-complexos/script.js
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-Javascript/Exercicios-mais-complexos/script.js
@@ -1,3 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
+
+let resultado = 0;
+
+for(let index=0; index<numbers.length; index+=1){
+    resultado += numbers[index];
+}
+
+console.log("A soma de todos os valores do array é:",resultado);
