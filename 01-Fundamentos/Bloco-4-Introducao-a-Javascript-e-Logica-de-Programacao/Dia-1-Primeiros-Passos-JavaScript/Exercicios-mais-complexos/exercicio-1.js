759c473d3d97697fde037f99eed64dd39ce19a1e
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-14 14:23:51 -0300

    Adiciona duas variaveis para realizar operacoes de soma, subtracao, multiplicacao, divisao e resto/modulo

---
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-JavaScript/Exercicios-mais-complexos/exercicio-1.js
@@ -0,0 +1,14 @@
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+const a = 10;
+const b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
