88cde28327958320fff01a109192d68f90844003
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-14 14:49:44 -0300

    Adiciona condicao para saber se o valor é positivo ou negativo

---
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-JavaScript/Exercicios-mais-complexos/exercicio-4.js
@@ -0,0 +1,11 @@
+//Verifica se o valor é positivo ou negativo
+
+const valor = 2;
+
+if (valor > 0){
+    console.log(valor,"é positivo");
+}else if (valor < 0){
+    console.log(valor,"é negativo");
+}else{
+    console.log("Valor 0");
+}
