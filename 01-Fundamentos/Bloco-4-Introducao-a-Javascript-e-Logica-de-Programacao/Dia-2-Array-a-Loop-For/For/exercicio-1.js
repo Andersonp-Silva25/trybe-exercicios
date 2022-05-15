2bb2473ab65239d849d84dee80463d655167c21b
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-15 11:24:53 -0300

    Adiciona for para mostrar todos os itens do array

---
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-2-Array-a-Loop-For/For/exercicio-1.js
@@ -0,0 +1,7 @@
+// Mostre todos os itens da lista utilizando for
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (index = 0; index < groceryList.length; index += 1){
+    console.log(groceryList[index]);
+}
