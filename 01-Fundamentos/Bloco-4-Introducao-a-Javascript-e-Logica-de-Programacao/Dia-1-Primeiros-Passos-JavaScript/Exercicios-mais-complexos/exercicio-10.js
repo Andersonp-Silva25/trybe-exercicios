45d51ec37d3577decb8cc12e809b11813ddf9fd3
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-15 19:44:41 -0300

    Verifica o lucro da venda de produtos ja com desconto de impostos

--- 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-JavaScript/Exercicios-mais-complexos/exercicio-10.js
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-1-Primeiros-Passos-JavaScript/Exercicios-mais-complexos/exercicio-10.js
@@ -1,7 +1,6 @@
 // Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
 // + Atente que, sobre o custo do produto, incide um imposto de 20%.
 
-
 const custo = 15;
 const valor = 35;
 
