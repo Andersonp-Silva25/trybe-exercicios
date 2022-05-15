59d23c823f2371d3ba4068025fb43e0b68afe886
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-15 11:38:09 -0300

    Corrigindo variavel dentro do For

--- 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-2-Array-a-Loop-For/For/exercicio-1.js
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-2-Array-a-Loop-For/For/exercicio-1.js
@@ -2,6 +2,6 @@
 
 let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for (index = 0; index < groceryList.length; index += 1){
+for (let index = 0; index < groceryList.length; index += 1){
     console.log(groceryList[index]);
 }
