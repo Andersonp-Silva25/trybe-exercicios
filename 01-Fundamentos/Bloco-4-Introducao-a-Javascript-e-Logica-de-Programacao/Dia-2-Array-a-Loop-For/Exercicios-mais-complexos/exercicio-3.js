4776d560e4ae2fa7364e4d5c7aa6df85975af823
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-15 16:18:58 -0300

    Adiciona media aritimetica para a soma dos valores do array

---
+++ 01-Fundamentos/Bloco-4-Introducao-a-Javascript-e-Logica-de-Programacao/Dia-2-Array-a-Loop-For/Exercicios-mais-complexos/exercicio-3.js
@@ -0,0 +1,13 @@
+// calculando a media aritimetica dos valores do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
+
+for (let index = 0; index < numbers.length; index += 1){
+    soma += numbers[index];
+}
+
+media = soma / numbers.length;
+
+console.log(media);
