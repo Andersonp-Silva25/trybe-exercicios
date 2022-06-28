f64eb4944774e3f3bd5413cef846058044faaa57
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-06-28 20:15:10 -0300

    Exercicio 8

---
+++ 01-Fundamentos/Bloco-8-High-Order-Functions-HOFS/Dia-5-SpreadOperator-ParametroRest-Destructuring-e-mais/exercicio-8.js
@@ -0,0 +1,6 @@
+//Retorne uma mensagem de cumprimeto utilizando default params
+const greet = (name, greeting = 'Hi') => ` ${greeting} ${name}`
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
